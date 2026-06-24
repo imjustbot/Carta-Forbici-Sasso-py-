dd6c7bd7b12f7b86ff80e4ed6dfa513ca20b63c1
imjustbot <nico7tolu@gmail.com>
2026-06-24 23:23:58 +0200

    Finito Carta, Forbici, Sasso

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
 import random
 import streamlit as st
 
+WPLAYER = "Ha vinto il giocatore!"
+
+WPC = "Ha vinto il computer!"
+
+MOSSE = ["CARTA", "FORBICI", "SASSO"]
+
+st.title("CARTA, FORBICI, SASSO")
+
+mossa_utente = st.selectbox("SCEGLI LA TUA RISPOSTA:", MOSSE)
+
+mossa_computer = random.choice(MOSSE)
+
+st.write(f"Il computer ha scelto {mossa_computer}")
+
+# VITTORIE DELL'UTENTE:
+
+if mossa_utente == "CARTA" and mossa_computer == "SASSO":
+
+    st.write(WPLAYER)
+
+elif mossa_utente == "SASSO" and mossa_computer == "FORBICI":
+
+    st.write(WPLAYER)
+
+elif mossa_utente == "FORBICI" and mossa_computer == "CARTA":
+
+    st.write(WPLAYER)
+
+# VITTORIE DEL PC:
+
+elif mossa_utente == "SASSO" and mossa_computer == "CARTA":
+
+    st.write(WPC)
+
+elif mossa_utente == "FORBICI" and mossa_computer == "SASSO":
+
+    st.write(WPC)
+
+elif mossa_utente == "CARTA" and mossa_computer == "FORBICI":
+
+    st.write(WPC)
+
+# IL PAREGGIO:
+
+else:
+
+    st.write("Pareggio!")
+
+
